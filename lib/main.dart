c508682a6fdff61ff30bd8b8d15a6fb5b302c6c6
ME <rajvardhanpatil002@gmail.com>
2023-02-16 12:51:54 +0530

    Added screen orientation.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:r_player/player.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MaterialApp(home: Player()));
 }
 
--- lib/player.dart
+++ lib/player.dart
@@ -3,7 +3,7 @@ import 'dart:io' as io;
 import 'package:file/memory.dart';
 import 'package:file/file.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_volume/flutter_volume.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 import 'package:volume_controller/volume_controller.dart';
 
@@ -18,16 +18,12 @@ class _PlayerState extends State<Player> {
   late VideoPlayerController _controller;
   double _playbacktime = 0, _volumecontroller = 0;
 
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
 
   @override
   void initState() {
     super.initState();
-
+    // SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeRight,DeviceOrientation.landscapeLeft,]);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       FileSystem fs = MemoryFileSystem(style: FileSystemStyle.posix);
       // log((await io.File("storage/emulated/0/download/govinda.mkv").exists()).toString());
@@ -37,7 +33,7 @@ class _PlayerState extends State<Player> {
         ..setVolume(_volumecontroller)
         ..addListener(() {
           setState(() {
-            _playbacktime = _controller.value.position.inSeconds.toDouble();
+            // _playbacktime = _controller.value.position.inSeconds.toDouble();
           });
         })
         ..initialize().then((_) async {
@@ -58,95 +54,140 @@ class _PlayerState extends State<Player> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _controller.value.isPlaying ? _controller.pause() : _controller.play();
-          });
-        },
-        child: Icon(_controller.value.isPlaying ? (Icons.pause) : (Icons.play_arrow)),
-      ),
-      body: Stack(
-        children: [
-          Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Current time : $_playbacktime"),
-              AspectRatio(aspectRatio: _controller.value.aspectRatio, child: VideoPlayer(_controller)),
-              // IconButton(onPressed: ()async{
-              //   VolumeController().muteVolume();
-              //   await _controller.setVolume(0.0);
-              //   setState(() {
-              //
-              //   });
-              // }, icon: Icon(Icons.volume_mute))
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: GestureDetector(
-                  onDoubleTap: () {
-                    setState(() {
-                      _playbacktime = _playbacktime - 10;
-                      _controller.seekTo(Duration(seconds: _playbacktime.toInt() - 10));
-                    });
-                  },
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _controller.value.isPlaying ? _controller.pause() : _controller.play();
+            });
+          },
+          child: Icon(_controller.value.isPlaying ? (Icons.pause) : (Icons.play_arrow)),
+        ),
+        body: Stack(
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AspectRatio(aspectRatio: _controller.value.aspectRatio, child: VideoPlayer(_controller)),
+                // IconButton(onPressed: ()async{
+                //   VolumeController().muteVolume();
+                //   await _controller.setVolume(0.0);
+                //   setState(() {
+                //
+                //   });
+                // }, icon: Icon(Icons.volume_mute))
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    child: Container(
+                      color: Colors.red,
+                    ),
+                    onVerticalDragUpdate:(update) {
+                      log("${update.delta.dx}         ${update.delta.dy}");
+                      if (update.delta.dy.isNegative) {
+                        if ((_volumecontroller - (update.delta.dy.toDouble() / 100)) <= 1) {
+                          _volumecontroller -= (update.delta.dy.toDouble() / 100);
+                        } else {
+                          _volumecontroller = 1;
+                        }
+                      } else {
+                        if ((_volumecontroller - (update.delta.dy.toDouble() / 100)) >= 0) {
+                          _volumecontroller -= (update.delta.dy.toDouble() / 100);
+                        } else {
+                          _volumecontroller = 0;
+                        }
+                      }
+                      setState(() {
+                        VolumeController().setVolume(_volumecontroller, showSystemUI: true);
+                      });
+                      log("$_volumecontroller");
+                    },
+                    onDoubleTap: () {
+                      setState(() {
+                        _playbacktime = _playbacktime - 10;
+                        _controller.seekTo(Duration(seconds: _playbacktime.toInt() - 10));
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 3,
-                child: GestureDetector(
-                  onHorizontalDragUpdate: (up) {
-                    log((up.delta.dx / 10).toString());
-                    setState(() {
-                      _playbacktime = _playbacktime + (up.delta.dx / 10);
-                      _controller.seekTo(Duration(seconds: (_playbacktime).toInt()));
-                    });
-                  },
+                Expanded(
+                  flex: 3,
+                  child: GestureDetector(
+                    child: Container(
+                      // color: Colors.green,
+                    ),
+                    onHorizontalDragUpdate: (update) {
+                      log((update.delta.dx / 10).toString());
+                      setState(() {
+                        _playbacktime = _playbacktime + (update.delta.dx / 10);
+                        _controller.seekTo(Duration(seconds: (_playbacktime).toInt()));
+                      });
+                    },
+                  ),
                 ),
-              ),
-              Expanded(
-                child: GestureDetector(
-                  onDoubleTap: () {
-                    setState(() {
-                      _playbacktime = _playbacktime + 10;
-                      _controller.seekTo(Duration(seconds: _playbacktime.toInt() + 10));
-                    });
-                  },
+                Expanded(
+                  child: GestureDetector(
+                    child: Container(
+                      // color: Colors.blue,
+                    ),
+                    onDoubleTap: () {
+                      setState(() {
+                        _playbacktime = _playbacktime + 10;
+                        _controller.seekTo(Duration(seconds: _playbacktime.toInt() + 10));
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Slider(
+                    value: _playbacktime,
+                    min: 0,
+                    max: _controller.value.duration.inSeconds.toDouble(),
+                    onChanged: (onChanged) {
+                      setState(() {
+                        _playbacktime = onChanged.floorToDouble();
+                        _controller.seekTo(Duration(seconds: onChanged.toInt()));
+                      });
+                    }),
+                // Slider(
+                //     value: _volumecontroller,
+                //     min: 0,
+                //     max: 1,
+                //     onChanged: (onChanged) {
+                //       setState(() {
+                //         _volumecontroller = onChanged;
+                //         VolumeController().setVolume(_volumecontroller, showSystemUI: false);
+                //       });
+                //     }),
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              )
-            ],
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Slider(
-                  value: _playbacktime,
-                  min: 0,
-                  max: _controller.value.duration.inSeconds.toDouble(),
-                  onChanged: (onChanged) {
-                    setState(() {
-                      _playbacktime = onChanged.floorToDouble();
-                      _controller.seekTo(Duration(seconds: onChanged.toInt()));
-                    });
-                  }),
-              Slider(
-                  value: _volumecontroller,
-                  min: 0,
-                  max: 1,
-                  onChanged: (onChanged) {
-                    setState(() {
-                      _volumecontroller = onChanged;
-                      VolumeController().setVolume(_volumecontroller, showSystemUI: false);
-                    });
-                  }),
-            ],
-          )
-        ],
-      ),
-    );
+                Text("Current time : ${_controller.value.position}"),
+                Text("Current volume : $_volumecontroller"),
+              ],
+            ),
+          ],
+        ));
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp,DeviceOrientation.landscapeRight, DeviceOrientation.landscapeRight,]);
+  }
+
 }
